d9fd4142c8e24df2dbab810b9c90a7055cca53b1
Robson Junior <robsonjunior1994@gmail.com>
2020-12-10 00:12:36 -0300

    Encapsulando metodo de criar token e enviar email como um metodo da classe token para ser usado em outro endpoint

--- ApiHeimdall/ApiHeimdall/Models/Usuario.cs
+++ ApiHeimdall/ApiHeimdall/Models/Usuario.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ApiHeimdall.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -24,5 +25,6 @@ namespace ApiHeimdall.Models
             }
             return true;
         }
+
     }
 }
