05b041fe918dee60a9dd69dc5c57915172defb9c
Eneas <lucasmartins_eu@hotmail.com>
2020-12-09 22:03:44 -0300

    Token implementado no Cadastro de usuario, testes de cadastro criados e funcionando

--- ApiHeimdall/ApiHeimdall/Controllers/UsuarioController.cs
+++ ApiHeimdall/ApiHeimdall/Controllers/UsuarioController.cs
@@ -5,20 +5,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ApiHeimdall.Controllers;
 
 namespace ApiHeimdall.Controllers
 {
     public class UsuarioController : Controller
     {
-        readonly IUsuarioRepository _usuarioRepository;
-        public UsuarioController(IUsuarioRepository usuarioRepository)
+        private readonly IUsuarioRepository _usuarioRepository;
+        private readonly ITokenRepository _tokenRepository;
+        
+        public UsuarioController(IUsuarioRepository usuarioRepository, ITokenRepository tokenRepository)
         {
             _usuarioRepository = usuarioRepository;
+            _tokenRepository = tokenRepository;
         }
 
         [Route("api/[controller]")]
         [HttpPost]
-        public ActionResult CadastroUsuario([FromBody] Usuario usuario)
+        public ActionResult Post([FromBody] Usuario usuario)
         {
             
             if (!(usuario.eUsuarioValido()))
@@ -34,6 +38,8 @@ namespace ApiHeimdall.Controllers
             else
             {
                 _usuarioRepository.Cadastrar(usuario);
+                TokenController tokenController = new TokenController(_tokenRepository, _usuarioRepository);
+                tokenController.Post(usuario.Email);
             }
             return Ok(usuario);
         }
---
+++ ApiHeimdall/XUnitTestProject/UsuarioTests.cs
@@ -0,0 +1,103 @@
+﻿using ApiHeimdall.Controllers;
+using ApiHeimdall.Interfaces;
+using ApiHeimdall.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject
+{
+    public class UsuarioTests
+    {
+        [Trait("UsuarioController", "CadastrarUsuario")]
+        [Fact(DisplayName = "01-DeveriaSalvarUsuario")]
+        public void DeveriaSalvarUsuario()
+        {
+            //Arrange
+            var UsuarioRepositoryMock = new Mock<IUsuarioRepository>();
+            var TokenRepositoryMock = new Mock<ITokenRepository>();
+            var sutUsuarioController = new UsuarioController(UsuarioRepositoryMock.Object, TokenRepositoryMock.Object);
+            var sutTokenController = new TokenController(TokenRepositoryMock.Object, UsuarioRepositoryMock.Object);
+
+            Usuario usuarioFake = new Usuario()
+            {
+                Email = "robsonjunior1994@gmail.com",
+                Ativo = false,
+                NomeCompleto = "Eneas Martins",
+                Senha = "eneas12345",
+                UserName = "eneasmartins"
+            };
+            UsuarioRepositoryMock.Setup(u => u.Cadastrar(usuarioFake));
+            UsuarioRepositoryMock.Setup(u => u.BuscarPorEmail(usuarioFake.Email)).Returns(usuarioFake);
+            
+            //Act
+            var retorno = sutUsuarioController.Post(usuarioFake) as OkObjectResult;
+
+            //Assert
+            Assert.True(retorno.StatusCode == 200);
+        }
+
+        [Trait("UsuarioController", "CadastrarUsuario")]
+        [Theory(DisplayName = "03-DeveriaNaoSalvarQuandoUsuarioInvalido")]
+        [InlineData("", "Eneas Martins", "eneas12345", "eneasmartins")]
+        [InlineData("robsonjunior1994@gmail.com", "", "eneas12345", "eneasmartins")]
+        [InlineData("robsonjunior1994@gmail.com", "Eneas Martins", "", "eneasmartins")]
+        [InlineData("robsonjunior1994@gmail.com", "Eneas Martins", "eneas12345", "")]
+        public void DeveriaNaoSalvarSeUsuarioInvalido(string email, string nome, string senha, string user)
+        {
+            //Arrange
+            var UsuarioRepositoryMock = new Mock<IUsuarioRepository>();
+            var TokenRepositoryMock = new Mock<ITokenRepository>();
+            var sutUsuarioController = new UsuarioController(UsuarioRepositoryMock.Object, TokenRepositoryMock.Object);
+            var sutTokenController = new TokenController(TokenRepositoryMock.Object, UsuarioRepositoryMock.Object);
+
+            Usuario usuarioFake = new Usuario()
+            {
+                Email = email,
+                Ativo = false,
+                NomeCompleto = nome,
+                Senha = senha,
+                UserName = user
+            };
+            UsuarioRepositoryMock.Setup(u => u.Cadastrar(usuarioFake));
+
+            //Act
+            var retorno = sutUsuarioController.Post(usuarioFake) as BadRequestResult;
+
+            //Assert
+            Assert.True(retorno.StatusCode == 400);
+        }
+
+        [Trait("UsuarioController", "CadastrarUsuario")]
+        [Fact(DisplayName = "02-DeveriaNaoSalvarQuandoUsuarioExiste")]
+        public void DeveriaNaoSalvarQuandoUsuarioExiste()
+        {
+            //Arrange
+            var UsuarioRepositoryMock = new Mock<IUsuarioRepository>();
+            var TokenRepositoryMock = new Mock<ITokenRepository>();
+            var sutUsuarioController = new UsuarioController(UsuarioRepositoryMock.Object, TokenRepositoryMock.Object);
+            var sutTokenController = new TokenController(TokenRepositoryMock.Object, UsuarioRepositoryMock.Object);
+
+            Usuario usuarioFake = new Usuario()
+            {
+                Id = 1,
+                Email = "robsonjunior1994@gmail.com",
+                Ativo = false,
+                NomeCompleto = "Eneas Martins",
+                Senha = "eneas12345",
+                UserName = "eneasmartins"
+            };
+            UsuarioRepositoryMock.Setup(u => u.Cadastrar(usuarioFake));
+            UsuarioRepositoryMock.Setup(u => u.Buscar(usuarioFake.Id)).Returns(usuarioFake);
+
+            //Act
+            var retorno = sutUsuarioController.Post(usuarioFake) as BadRequestResult;
+
+            //Assert
+            Assert.True(retorno.StatusCode == 400);
+        }
+    }
+}
